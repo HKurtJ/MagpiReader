d7b1a79d7b748cba16735fd1596dfc219be5e2a0
hkurt <hkurtj11@gmail.com>
2018-03-29 15:06:23 -0500

    Initial commit

---
+++ app/src/main/java/assinment/kierra_and_kurt/com/magpireader/MainActivity.java
@@ -0,0 +1,13 @@
+package assinment.kierra_and_kurt.com.magpireader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
