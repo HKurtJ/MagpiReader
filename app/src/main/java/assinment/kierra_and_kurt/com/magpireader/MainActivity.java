40922b42743142973bb2554e40277be43c7553e1
DangerRanger7 <kierradachelle@yahoo.com>
2018-04-21 20:36:54 -0500

    added another activity

--- app/src/main/java/assinment/kierra_and_kurt/com/magpireader/MainActivity.java
+++ app/src/main/java/assinment/kierra_and_kurt/com/magpireader/MainActivity.java
@@ -1,7 +1,9 @@
 package assinment.kierra_and_kurt.com.magpireader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.start_button);
-        findViewById(R.id.about_button);
+        findViewById(R.id.start_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ReadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.about_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
